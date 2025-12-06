4291e4340ce7c92d98e141a2d314c4fd4e591891
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-12-06 21:59:12 +1300

    initialise slider

---
+++ script.js
@@ -0,0 +1,14 @@
+// IMAGE SLIDER
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+
+const initialiseSlider = () => {
+  slides[slideIndex].classList.add("displaySlide");
+};
+
+const showSlide = (index) => {};
+
+const prevSlide = () => {};
+const nextSlide = () => {};
+initialiseSlider();
